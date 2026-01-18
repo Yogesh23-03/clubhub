3a22d21f74fca6608494cf627971d8f36c58d3ee
Yogesh S <116181840+Yogesh23-03@users.noreply.github.com>
2026-01-18 16:24:38 +0530

    Update settings.py

--- clubhub/settings.py
+++ clubhub/settings.py
@@ -20,11 +20,7 @@ SECRET_KEY = config('SECRET_KEY', default='unsafe-secret-key-change-me')
 
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = config(
-    'ALLOWED_HOSTS',
-    default='localhost,127.0.0.1',
-    cast=Csv()
-)
+ALLOWED_HOSTS =*
 
 
 # --------------------------------------------------
