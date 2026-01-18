2275fdd02b2689fd3f2ef6ba01fb3f69824e3acd
Yogesh S <116181840+Yogesh23-03@users.noreply.github.com>
2026-01-18 19:24:44 +0530

    Fix formatting of DEBUG configuration in settings.py

--- clubhub/settings.py
+++ clubhub/settings.py
@@ -18,7 +18,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # --------------------------------------------------
 SECRET_KEY = config('SECRET_KEY', default='unsafe-secret-key-change-me')
 
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = config("DEBUG", cast=bool, default=False)
+
 
 ALLOWED_HOSTS = ["*"]
 
